f1667b33e60b3210ba327121b38a42795837f4cf
Mma Obiora <101875261+Theecypher@users.noreply.github.com>
2023-09-08 21:33:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
 const day = document.querySelector("#day"),
 time = document.querySelector("#time");
 
-const months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
-
-function displayTime () {
     const d = new Date();
     const Day = d.getDay();
 let hours = d.getUTCHours();
 let minute = d.getUTCMinutes();
 let second = d.getUTCSeconds();
-    const Month = months[d.getMonth()];
-    const year = d.getFullYear();
+    const Month = d.getUTCMonth();
+    const year = d.getUTCFullYear();
+const millisecond = d.getUTCMilliseconds();
 
 
     hours = hours < 10 ? `0${hours}` : `${hours}`;
     minute = minute < 10 ? `0${minute}` : `${minute}`;
     second = second < 10 ? `0${second}` : `${second}`;
 
-    let currentTime = `${hours}:${minute}:${second}`;
+    let currentTime = `${year}${Month}${hours}:${minute}:${second}{millisecond}`;
     day.innerHTML = `${Day}th ${Month} ${year}`
 
     time.innerHTML = currentTime;
-}
-
-setInterval(displayTime, 1000);
 
 
 
