c273559ea05e84fd8922ac1474eb0201befa5ed3
Theecypher <obioramma12@gmail.com>
2023-09-08 21:26:58 +0100

    updated script.js

--- script.js
+++ script.js
@@ -2,37 +2,25 @@ const day = document.querySelector("#day"),
 time = document.querySelector("#time");
 
 const months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
+// const says= [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday];
 
-function displayTime () {
-    const d = new Date();
-    const Day = d.getDay();
+const d = new Date();
+const Day = d.getDay();
 let hours = d.getUTCHours();
 let minute = d.getUTCMinutes();
 let second = d.getUTCSeconds();
-    const Month = months[d.getMonth()];
-    const year = d.getFullYear();
+const Month = d.getUTCMonth();
+const year = d.getUTCFullYear();
+const millisecond = d.getUTCMilliseconds();
 
-    if (hours > 12) {
-        hours -= 12;
-        period = "PM"
-    }
-
-    if (hours == 0) {
-        hours = 12;
-        period = "AM"
-    }
 
     hours = hours < 10 ? `0${hours}` : `${hours}`;
     minute = minute < 10 ? `0${minute}` : `${minute}`;
     second = second < 10 ? `0${second}` : `${second}`;
 
-    let currentTime = `${hours}:${minute}:${second}`;
-    day.innerHTML = `${Day}th ${Month} ${year}`
+    let currentTime = `Current UTC Time: ${year}${Month}${hours}${minute}${second}${millisecond}`;
 
     time.innerHTML = currentTime;
-}
-
-setInterval(displayTime, 1000);
 
 
 
