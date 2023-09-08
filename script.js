e8417e898253d2b00ea80768ad0102d66dced5af
Theecypher <obioramma12@gmail.com>
2023-09-08 19:54:19 +0100

    new changes

--- script.js
+++ script.js
@@ -7,29 +7,28 @@ const d = new Date()
 
 function displayTime () {
     const d = new Date();
-    let hours = d.getHours();
-    let minute = d.getMinutes();
-    let second = d.getSeconds();
-    let period = 'AM';
+    let hours = d.getUTCHours();
+    let minute = d.getUTCMinutes();
+    let second = d.getUTCSeconds();
     const Day = d.getDay();
     const Month = months[d.getMonth()];
     const year = d.getFullYear();
 
-    if (hours > 12) {
-        hours -= 12;
-        period = "PM"
-    }
+    // if (hours > 12) {
+    //     hours -= 12;
+    //     period = "PM"
+    // }
 
-    if (hours == 0) {
-        hours = 12;
-        period = "AM"
-    }
+    // if (hours == 0) {
+    //     hours = 12;
+    //     period = "AM"
+    // }
 
     hours = hours < 10 ? `0${hours}` : `${hours}`;
     minute = minute < 10 ? `0${minute}` : `${minute}`;
     second = second < 10 ? `0${second}` : `${second}`;
 
-    let currentTime = `${hours} ${minute} ${second} ${period}`;
+    let currentTime = `${hours} ${minute} ${second}`;
     day.innerHTML = `${Day}th ${Month} ${year}`
 
     time.innerHTML = currentTime;
