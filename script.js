ac9a0fc19dcd802445766ab6a91ae92a7e3adcaf
Theecypher <obioramma12@gmail.com>
2023-09-08 13:21:49 +0100

    new changes

--- script.js
+++ script.js
@@ -4,16 +4,39 @@ time = document.querySelector("#time");
 const months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
 
 const d = new Date()
-const Day = d.getDay();
-const Month = months[d.getMonth()];
-const year = d.getFullYear();
-const hours = d.getHours();
-const minute = d.getMinutes();
-const second = d.getSeconds();
 
-// if ()
+function displayTime () {
+    const d = new Date();
+    let hours = d.getHours();
+    let minute = d.getMinutes();
+    let second = d.getSeconds();
+    let period = 'AM';
+    const Day = d.getDay();
+    const Month = months[d.getMonth()];
+    const year = d.getFullYear();
 
-time.innerHTML = `${hours}:0${minute}:${second}`
-day.innerHTML = `${Day}th ${Month} ${year}`
+    if (hours > 12) {
+        hours -= 12;
+        period = "PM"
+    }
+
+    if (hours == 0) {
+        hours = 12;
+        period = "AM"
+    }
+
+    hours = hours < 10 ? `0${hours}` : `${hours}`;
+    minute = minute < 10 ? `0${minute}` : `${minute}`;
+    second = second < 10 ? `0${second}` : `${second}`;
+
+    let currentTime = `${hours} ${minute} ${second} ${period}`;
+    day.innerHTML = `${Day}th ${Month} ${year}`
+
+    time.innerHTML = currentTime;
+}
+
+setInterval(displayTime, 1000);
+
+
 
 
