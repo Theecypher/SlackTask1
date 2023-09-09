c2e97af28b32a06a68ae0a8c5bad92f5edfe6f21
Mma Obiora <101875261+Theecypher@users.noreply.github.com>
2023-09-09 10:32:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ time = document.querySelector("#time");
 
     const d = new Date();
     const Day = d.getDay();
-let hours = d.getUTCHours();
-let minute = d.getUTCMinutes();
-let second = d.getUTCSeconds();
-    const month = d.getUTCMonth();
-    const year = d.getUTCFullYear();
-const millisecond = d.getUTCMilliseconds();
+const times = d.getTime();
+//let hours = d.getUTCHours();
+//let minute = d.getUTCMinutes();
+//let second = d.getUTCSeconds();
+   // const month = d.getUTCMonth();
+    //const year = d.getUTCFullYear();
+//const millisecond = d.getUTCMilliseconds();
 
-    hours = hours < 10 ? `0${hours}` : `${hours}`;
-    minute = minute < 10 ? `0${minute}` : `${minute}`;
-    second = second < 10 ? `0${second}` : `${second}`;
+    //hours = hours < 10 ? `0${hours}` : `${hours}`;
+    //minute = minute < 10 ? `0${minute}` : `${minute}`;
+    //second = second < 10 ? `0${second}` : `${second}`;
 
-    let currentTime = `Current UTC Time: ${year}${month}${hours}${minute}${second}${millisecond}`;
+    //let currentTime = `Current UTC Time: ${year}${month}${hours}${minute}${second}${millisecond}`;
     day.innerHTML = "Thursday";
 
-    time.innerHTML = currentTime;
+    time.innerHTML = ${times};
 
 
 
